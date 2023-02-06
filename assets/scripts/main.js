ea6e3edc20ca996138f7ba4d74d04e40bda68718
Level7までをPush <>
2023-02-07 07:49:52 +0900

    Lv2までコミット(修正)

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -5,6 +5,7 @@ import '@splidejs/splide/css/skyblue';
 import Splide from '@splidejs/splide';
 import ActiveNav from './modules/activeNav';
 
+
 if(document.getElementById('js-nav') !== null){
 	new ActiveNav({
 		navElement: 'js-nav'
