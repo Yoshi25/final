f8ee8b75124c2d7e05924de20b26c598aa597903
Level7までをPush <>
2023-02-06 19:47:43 +0900

    lv2までコミット

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,13 +1,16 @@
-// デフォルトのテーマ
+import '../styles/main.scss'
 import '@splidejs/splide/css';
-
-// または、そのほかのテーマ
-import '@splidejs/splide/css/skyblue';
-import '@splidejs/splide/css/sea-green';
-
-// あるいは、コアスタイルのみ
 import '@splidejs/splide/css/core';
-
+import '@splidejs/splide/css/skyblue';
 import Splide from '@splidejs/splide';
+import ActiveNav from './modules/activeNav';
 
-new Splide( '.splide' ).mount();
+if(document.getElementById('js-nav') !== null){
+	new ActiveNav({
+		navElement: 'js-nav'
+	})
+}
+
+if(document.getElementsByClassName('splide')[0]){
+	new Splide( '.splide' ).mount();
+}
